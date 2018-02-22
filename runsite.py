a3752825ef5b62f9616a95c8b9f743b21e101c1b
Kat Matfield <katmatfield@gmail.com>
2018-02-22 14:34:27 +0000

    First few attempts

--- runsite.py
+++ runsite.py
@@ -8,6 +8,45 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+# attempt 1
+@app.route('/local')
+def local():
+    return render_template('local.html')
+ 
+# attempt 2
+@app.route('/remote')
+def remote():
+    return render_template('remote.html')
+
+# attempt 3
+@app.route('/googles-image')
+def googles_image():
+    return render_template('googles-image.html')
+ 
+# attempt 4
+@app.route('/tweak')
+def tweak():
+    return render_template('tweak.html')
+ 
+# attempt 5
+@app.route('/aws')
+def aws():
+    return render_template('aws.html')
+
+# attempt 6
+@app.route('/aws-tweak')
+def aws_tweak():
+    return render_template('aws-tweak.html')
+
+# attempt 7
+@app.route('/aws-change-filepath')
+def aws_change_filepath():
+    return render_template('aws-change-filepath.html')
+
+# @app.route('/<page>')
+# def section(page=None):
+#     return render_template('{0}.html'.format(page), page=page)
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('404.html'), 404
